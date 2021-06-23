ca92fd92c074c928632c6586f3f68e8e67efde51
jaredl85 <jaredloveridge@gmail.com>
2021-06-23 16:36:13 -0700

    Added header & footer components, fixed bug in CampsiteInfo component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent.js';
 import { CAMPSITES } from '../shared/campsites';
 import CampsiteInfo from './CampsiteInfoComponent.js';
+import Header from './HeaderComponent.js';
+import Footer from './FooterComponent.js';
 
 class Main extends Component {
     constructor(props) {
@@ -20,13 +21,10 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <Navbar dark color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/">Nucamp</NavbarBrand>
-                    </div>
-                </Navbar>
+                <Header />
                 <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Footer />
             </div>
         );
     }
